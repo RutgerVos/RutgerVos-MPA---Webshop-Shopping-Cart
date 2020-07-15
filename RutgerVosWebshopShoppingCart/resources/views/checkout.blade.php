558ed793a02326c5b7c2ac9094596d7442bc02ec
Rutger Vos <99033234@mydavinci.nl>
2020-07-15 12:59:24 +0200

    made base for check functionns

---
+++ RutgerVosWebshopShoppingCart/resources/views/checkout.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title')
+Checkout
+@endsection
+@section('content')
+<p>Checkout done</p>
+<a type="button" href="" class='btn btn-info'value="">profile</a>
+@endsection
